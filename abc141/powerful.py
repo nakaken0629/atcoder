8d420ffa1d5d4a6e372b062542ace73f7840d87c
NAKAGAKI Kenji <nakaken0629@gmail.com>
2019-09-16 07:11:46 +0900

    D問題を時間内で解けるように修正

--- abc141/powerful.py
+++ abc141/powerful.py
@@ -1,17 +1,17 @@
 import math
-import bisect
+import heapq
 
 N, M = list(map(int, input().split()))
-A = list(map(int, input().split()))
+A = list(map(lambda a: -int(a), input().split()))
 A.sort()
+heapq.heapify(A)
 
 for i in range(M):
-  if (N > 1 and A[-1] < A[-2]):
-    a = A.pop(-1)
-    bisect.insort(A, a)
-  A[-1] /= 2.0
+  a = heapq.heappop(A)
+  a /= 2.0
+  heapq.heappush(A, a)
 
 amount = 0
 for a in A:
-  amount += math.floor(a)
+  amount += math.floor(-a)
 print(amount)
