49036e8ea9b5533bdb179fcc15c609c13a28e3d0
NAKAGAKI Kenji <nakaken0629@gmail.com>
2019-11-16 23:28:43 +0900

    便利関数を追加

---
+++ __util/util.py
@@ -0,0 +1,13 @@
+# nCk をmodで割った余りを求める
+def mod_combination(n, k, mod = 10 ** 9 + 7):
+    def extended_gcd(a, b):
+        if b == 0:
+            return (a, 1, 0)
+        d, x, y = extended_gcd(b, a % b)
+        return (d, y, x - (a // b) * y)
+    p, q = 1, 1
+    for i in range(n - k + 1, n + 1):
+        p = (p * i) % mod
+    for i in range(2, k + 1):
+        q = (q * i) % mod
+    return p * (extended_gcd(q, mod)[1] % mod) % mod
