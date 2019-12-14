9f43c809cc2e57b9b4d6047b94f685efe07472a7
NAKAGAKI Kenji <nakaken0629@gmail.com>
2019-12-14 20:28:09 +0900

    作業途中だけどコミット

--- nikkei2019-2-qual/swap.py
+++ nikkei2019-2-qual/swap.py
@@ -7,3 +7,9 @@ B = list(map(int, input().split()))
 sorted_AB = sorted(zip(B, A))
 sorted_A, sorted_B = zip(*sorted_AB)
 print(sorted_A, sorted_B)
+
+swaps = 0
+for i in range(N):
+    if sorted_A[i] <= sorted_B[i]:
+        continue
+     
