57710adb2d9e89f61e1cdb4740071bd66837a7ba
NAKAGAKI Kenji <nakaken0629@gmail.com>
2020-03-06 01:07:10 +0900

    union_findを使ってABC157のDを実装

--- __util/union_find.py
+++ __util/union_find.py
@@ -27,33 +27,3 @@ class UnionFind:
     
     def size(self, x):
         return self.sizes[self.root(x)]
-
-N, M, K = list(map(int, input().split()))
-AB_list = []
-for _ in range(M):
-    A, B = list(map(int, input().split()))
-    AB_list.append([A - 1, B - 1])
-CD_list = []
-for _ in range(K):
-    C, D = list(map(int, input().split()))
-    CD_list.append([C - 1, D - 1])
-
-uf = UnionFind(N)
-for A, B in AB_list:
-    uf.unite(A, B)
-
-friends = [0] * N
-for A, B in AB_list:
-    friends[A] += 1
-    friends[B] += 1
-blocks = [0] * N
-for C, D in CD_list:
-    if uf.root(C) != uf.root(D):
-        continue
-    blocks[C] += 1
-    blocks[D] += 1
-answers = [0] * N
-for i in range(N):
-    answers[i] = uf.size(i) - 1 - friends[i] - blocks[i]
-
-print(" ".join(map(str, answers)))
